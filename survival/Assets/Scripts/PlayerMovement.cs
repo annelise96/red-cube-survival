b1a2957c2e699fcda3ff613bff45fad6ccc4472e
Anna Toth <annelise96@users.noreply.github.com>
2018-07-16 14:08:07 +0200

    Added project

---
+++ survival/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (FindObjectOfType<GameManager>().getGameHasEnded() == false)
+        {
+            if (Input.GetKey("d"))
+            {
+                rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            }
+            if (Input.GetKey("a"))
+            {
+                rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            }
+        }
+
+        if (rb.position.y < 0.95f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
---
+++ survival/Assets/Scripts/score.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour {
+
+    public Transform player;
+    public Text scoreText;
+	
+	// Update is called once per frame
+	void Update () {
+        if(FindObjectOfType<GameManager>().getGameHasEnded() == false)
+        {
+            scoreText.text = (0.1 *player.position.z).ToString("0");
+        }
+        
+	}
+}
