943932c69dc2adb75496446938a35eb944ff24d9
Anna Toth <annelise96@users.noreply.github.com>
2018-07-16 18:37:07 +0200

    blocks generate automatically

---
+++ survival/Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour {
+
+    public Transform player;
+    public Transform[] spawnPoints;
+    public GameObject blockPrefab;
+
+    public int wavesSpawned = 0;
+
+    void Update()
+    {
+        if(player.position.z >= wavesSpawned*75)
+        {
+            transform.position = new Vector3(0, 1, FindObjectOfType<PlayerMovement>().rb.position.z + 100);
+            SpawnBlocks();
+            wavesSpawned++;
+        }       
+    }
+
+    void SpawnBlocks ()
+    {
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            if (randomIndex != i)
+            {
+                Instantiate(blockPrefab, spawnPoints[i].position, Quaternion.identity);
+            }
+        }
+	}	
+}
